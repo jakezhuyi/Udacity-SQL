d9e30c9e1c1b0fb2e51f6792d5fa7d8b35371af0
OSSYULYYZ <yimeng.zhu@mail.mcgill.ca>
2020-08-03 14:54:26 -0400

    Database Revision 2

--- SQL-Udiddit/good_db.sql
+++ SQL-Udiddit/good_db.sql
@@ -118,3 +118,30 @@ SELECT
 FROM bad_comments
 JOIN users ON bad_comments.username = users.username
 JOIN posts ON posts.id = bad_comments.post_id;
+
+-- https://knowledge.udacity.com/questions/293663
+INSERT INTO "votes"
+(
+  "post_id",
+  "user_id",
+  "vote"
+)
+
+SELECT t1.id, users.id,
+1 AS vote_up
+FROM (SELECT id, REGEXP_SPLIT_TO_TABLE(upvotes,',') 
+  AS upvote_users FROM bad_posts) t1
+JOIN users ON users.username=t1.upvote_users;
+
+INSERT INTO "votes"
+(
+  "post_id",
+  "user_id",
+  "vote"
+)
+
+SELECT t1.id, users.id,
+-1 AS vote_down
+FROM (SELECT id, REGEXP_SPLIT_TO_TABLE(downvotes,',') 
+  AS downvote_users FROM bad_posts) t1
+JOIN users ON users.username=t1.downvote_users;
